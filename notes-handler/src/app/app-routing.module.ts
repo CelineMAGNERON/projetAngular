45b2522281937fbd79a8a7f3d5c38ad7c092c32d
Céline MAGNERON <59822501+CelineMAGNERON@users.noreply.github.com>
2021-06-18 11:38:36 +0200

    Add files via upload

---
+++ notes-handler/src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {MainComponent} from './main/main.component';
+import {LoginComponent} from './login/login.component';
+import {LoginGuardService} from './services/login-guard.service';
+import {AboutComponent} from './about/about.component';
+import {NotFoundComponent} from './not-found/not-found.component';
+import { EditProfilComponent } from './edit-profil/edit-profil.component';
+
+const routes: Routes = [
+  {path: 'home', component: MainComponent, canActivate: [LoginGuardService]},
+  {path: 'login', component: LoginComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'profil', component: EditProfilComponent, canActivate: [LoginGuardService]},
+  {path: '**', component: NotFoundComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+}
---
+++ notes-handler/src/app/app.module.ts
@@ -0,0 +1,44 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {MainComponent} from './main/main.component';
+import {FormsModule} from '@angular/forms';
+import {NoteComponent} from './note/note.component';
+import {NoteEditorComponent} from './note-editor/note-editor.component';
+import {NotesManagerService} from './services/notes-manager.service';
+import {NotesHeadComponent} from './notes-head/notes-head.component';
+import {LoginComponent} from './login/login.component';
+import {NavbarComponent} from './navbar/navbar.component';
+import {AboutComponent} from './about/about.component';
+import {NotFoundComponent} from './not-found/not-found.component';
+import {HttpClientModule} from '@angular/common/http';
+import { EditProfilComponent } from './edit-profil/edit-profil.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainComponent,
+    NoteComponent,
+    NoteEditorComponent,
+    NotesHeadComponent,
+    LoginComponent,
+    NavbarComponent,
+    AboutComponent,
+    NotFoundComponent,
+    EditProfilComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [NotesManagerService],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
